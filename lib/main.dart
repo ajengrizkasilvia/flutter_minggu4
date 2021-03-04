9fc50cd0e83ae3093ef012d6f7f90ddbb50e2604
= <=>
2021-03-04 20:29:35 +0700

    jobsheet4

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'Convert.dart';
+import 'Input.dart';
+import 'Result.dart';
+import 'Riwayat.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final inputController = TextEditingController();
+  double _inputUser = 0;
+  double _kelvin = 0;
+  double _reamur = 0;
+  double _fahrenheit = 0;
+  String _newValue = "Kelvin";
+  double _result = 0;
+
+  List<String> listViewItem = List<String>();
+  var listItem = ["Kelvin", "Reamur", "Fahrenheit"];
+
+  void perhitunganSuhu() {
+    setState(() {
+      _inputUser = double.parse(inputController.text);
+      if (_newValue == "Kelvin")
+        _result = _inputUser + 273;
+      else if (_newValue == "Reamur")
+        _result = (4 / 5) * _inputUser;
+      else 
+        _result = (_inputUser * 9 / 5 ) + 32;
+    });
+    listViewItem.add("$_newValue : $_result");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Konverter Suhu"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Input(inputUserController: inputController),
+              DropdownButton<String>(
+                items: listItem.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                value: _newValue,
+                onChanged: (String changeValue) {
+                  setState(() {
+                    _newValue = changeValue;
+                    perhitunganSuhu();
+                  });
+                },
+              ),
+              Result(result: _result),
+              Convert(konvertHandler: perhitunganSuhu),
+                    Container(
+                      margin: EdgeInsets.only(top: 10, bottom: 10),
+                      child: Text(
+                        "Riwayat Konversi",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Expanded(
+                      child: Riwayat(listViewItem: listViewItem),
+                    ),
+                  ],
+                ),
+              ),
+      ),
+    );
+  }
+}
